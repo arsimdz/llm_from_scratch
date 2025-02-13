ea169fb423bdcb981f5daa4c8cd5beaf8805cd9e
arsimdz <darsim@student.ethz.ch>
2025-02-13 09:32:00 +0100

    top k sampling

--- train.py
+++ train.py
@@ -73,7 +73,7 @@ def generate_and_print_sample(model, tokenizer,device,start_context):
     context_size = model.pos_emb.weight.shape[0]
     encoded = text_to_token(start_context,tokenizer=tokenizer)
     with torch.no_grad():
-        token_ids = generate_text_simple(model=model,idx=encoded,context_size=context_size,max_new_tokens=50)
+        token_ids = generate(model=model,idx=encoded,context_size=context_size,max_new_tokens=50,temperature=2.0,top_k=3)
     gen_txt = token_ids_to_text(token_ids=token_ids,tokenizer=tokenizer)    
     print(gen_txt.replace("\n"," "))
     model.train() 
@@ -98,3 +98,30 @@ def text_to_token(text,tokenizer):
 def token_ids_to_text(token_ids,tokenizer):
     decode_txt = tokenizer.decode(token_ids.squeeze(0).tolist())
     return decode_txt
+
+
+def generate(model,idx,max_new_tokens,context_size,temperature=0.0,top_k=None,eos_id=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:,-context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:,-1,:]
+        
+        if top_k is not None:
+            top_k_logits, _ = torch.topk(logits,top_k)
+            logits = torch.where(
+            condition=logits < top_k_logits[:,-1],
+            input=torch.tensor(float('-inf')),
+            other=logits
+        )
+        if temperature > 0.0:
+            logits = logits/temperature
+            prob = torch.softmax(logits,dim=-1)
+            idx_next = torch.multinomial(prob,num_samples=1)
+            
+        else:
+            idx_next = torch.argmax(logits,dim=-1,keepdim=True)
+        if idx_next == eos_id:
+            break    
+        idx = torch.cat((idx,idx_next),dim=1)  
+    return idx    
