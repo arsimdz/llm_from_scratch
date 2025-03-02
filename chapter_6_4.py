27dccdf7b7322e41dcb6970bb7cc25f20577acbc
arsimdz <darsim@student.ethz.ch>
2025-03-02 23:29:44 +0100

    finetuning spam classification

--- chapter_6_4.py
+++ chapter_6_4.py
@@ -4,16 +4,24 @@ from gpt_config_124M import GPT_Config_124M as config
 from gpt import GPTModel
 import torch 
 import tiktoken
-
+from torch.utils.data import DataLoader
+from spam_data import SpamDataset
+import time
+from spam_data import train_classifier_simple
 model = GPTModel(config)
 tokenizer = tiktoken.get_encoding("gpt2")
 settings, params = download_and_load_gpt2(
     model_size="124M", models_dir="gpt2"
 )
 load_weights_into_gpt(model,params)
-model.eval()
 num_classes = 2
+
+for param in model.parameters():
+    param.requires_grad = False
+
 model.out_head = torch.nn.Linear(in_features=config["emb_dim"],out_features=num_classes)
+
+
 
 for param in model.trf_blocks[-1].parameters():
     param.requires_grad = True
@@ -21,10 +29,43 @@ for param in model.trf_blocks[-1].parameters():
 for param in model.final_norm.parameters():
     param.requires_grad = True
 
-inputs = tokenizer.encode("Do you have time.")
-inputs = torch.tensor(inputs).unsqueeze(0)
-with torch.no_grad():
-    outputs = model(inputs)
+train_dataset = SpamDataset(csv_file="train.csv",tokenizer=tokenizer)
+val_dataset = SpamDataset(csv_file="validation.csv",tokenizer=tokenizer,max_length=train_dataset.max_length)
+test_dataset = SpamDataset(csv_file="test.csv",tokenizer=tokenizer,max_length=train_dataset.max_length)
 
-print("output:\n",outputs)
-print("output dimension",outputs.shape)
+num_workers=0
+batch_size = 8
+
+train_loader  =  DataLoader(train_dataset,batch_size=batch_size,num_workers=num_workers
+                            ,shuffle=True,drop_last=True)
+
+test_loader = DataLoader(
+    test_dataset,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    drop_last=False)
+
+val_loader = DataLoader(
+    val_dataset,
+    batch_size=batch_size,
+    num_workers=num_workers,
+    drop_last=False)
+
+
+start_time = time.time()
+torch.manual_seed(123)
+optimizer = torch.optim.AdamW(model.parameters(),lr=5e-5,weight_decay=0.1)
+num_epochs = 5
+device = torch.device("cuda" if torch.cuda.is_available()else "cpu")
+
+train_losses,val_losses,train_accs,val_accs,examples = train_classifier_simple(
+    model,train_loader,val_loader,optimizer,device,num_epochs,eval_freq=50,eval_iter=5
+)
+
+torch.save(model.state_dict(), 'model_weights.pth')
+
+end_time = time.time()
+execution_time = (end_time-start_time)/60
+print(f"Training completed in {execution_time:.2f} minutes")
+
+
--- spam_data.py
+++ spam_data.py
@@ -1,6 +1,9 @@
 import torch
 from torch.utils.data import Dataset
 import pandas as pd 
+from torch.nn.functional import cross_entropy
+from train import evaluate_model
+
 class SpamDataset(Dataset):
     def __init__(self,csv_file,tokenizer,max_length=None,pad_token_id=50256):
         self.data = pd.read_csv(csv_file)
@@ -36,4 +39,102 @@ class SpamDataset(Dataset):
             if(max_length<encoded_length):
                 max_length = encoded_length
 
-        return max_length                  
+        return max_length  
+
+def calc_accuracy_loader(data_loader,model,device,num_batches=None):
+    model.eval()
+    correct_predictions,num_examples = 0, 0
+
+    if num_batches is None:
+        num_batches = len(data_loader)
+    else:
+        num_batches = min(num_batches,len(data_loader))
+
+    for i, (input_batch,target_batch) in enumerate(data_loader):
+        if i < num_batches:
+            input_batch = input_batch.to(device)
+            target_batch = target_batch.to(device)
+
+            with torch.no_grad():
+                logits = model(input_batch)[:,-1,:]
+            pred_label = torch.argmax(logits,dim=-1)
+            num_examples += pred_label.shape[0]
+            correct_predictions += (pred_label == target_batch).sum().item()
+        else:
+            break
+
+        return correct_predictions/num_examples 
+
+def calc_batch_loss(input_batch,target_batch,model,device):
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
+    logits = model(input_batch)[:,-1,:]
+    loss = cross_entropy(logits,target_batch)
+    return loss 
+
+
+
+def train_classifier_simple(model,train_loader,val_loader,
+                       optimizer,device,num_empochs,
+                       eval_freq,eval_iter):
+    
+    train_losses, val_losses, train_accs,val_accs = [],[],[],[]
+    examples_seen, global_step = 0,-1
+
+    for epoch in range(num_empochs):
+        model.train()
+        for input_batch,target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_batch_loss(input_batch,target_batch,model,device)
+            loss.backward()
+            optimizer.step()
+            examples_seen += input_batch.shape[0]
+            global_step += 1
+
+            if global_step % eval_freq ==0:
+                train_loss, val_loss = evaluate_model(model, train_loader,val_loader,device,eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                
+                print(f"Ep:{epoch+1} (Step {global_step:06d}): "
+                      f"Train loss:{train_loss:.3f}, "
+                       f"Val Loss: {val_loss:.3f}" )
+            
+        train_accuracy = calc_accuracy_loader(
+            train_loader,model,device, num_batches=eval_iter
+        )
+        val_accuracy = calc_accuracy_loader(
+            val_loader,model,device,num_batches=eval_iter
+        )
+
+        print(f"Training accuracy:{train_accuracy*100:.2f}%")
+        print(f"Validation accuracy: {val_accuracy*100:.2f}%")
+        train_accs.append(train_accuracy)
+        val_accs.append(val_accuracy)
+    return train_losses,val_losses,train_accs,val_accs,examples_seen  
+
+def calc_loss_loader(dataloader,model,device,num_batches=None):
+    loss = 0
+    if(len(dataloader)==0):
+        return float("nan")
+    elif(num_batches==None):
+        num_batches = len(dataloader)
+    else:
+        num_batches = min(num_batches,len(dataloader))
+    for i, (input_batch,target_batch) in enumerate(dataloader):
+        if(i<num_batches):
+            batch_loss = calc_batch_loss(input_batch,target_batch,model,device)
+            loss += batch_loss.item()
+        else:
+            break    
+
+        
+    return loss/num_batches  
+
+def evaluate_model(model, train_loader,val_loader,device,eval_iter):
+    model.eval()
+    with torch.no_grad():
+        val_loss = calc_loss_loader(dataloader=val_loader,model=model,device=device,num_batches=eval_iter)
+        train_loss = calc_loss_loader(dataloader=train_loader,model=model,device=device,num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
